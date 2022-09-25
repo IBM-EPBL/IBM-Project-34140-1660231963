2b76f25943a5cacfa2f3b47f095d781db2e98b70
Madhu Surya M <90973077+Dusky-Joker@users.noreply.github.com>
2022-09-25 16:39:04 +0530

    Add files via upload

---
+++ Assignments/Madhu Surya M/Assignment 2.py
@@ -0,0 +1,17 @@
+import random
+
+flag = 1
+
+while(flag==1 and flag!=2):
+    temperature = random.uniform(-80,190)
+    humidity = random.uniform(0,100)
+    if(temperature>=60):
+        print("Alert!!! Alarm ON!!! High temperature detected")
+        print("Temperature value is: "+str(temperature)+"° C")
+        print("Humidity value is: "+str(humidity))
+        print("Press 1 to turn off the alarm \nPress 2 to quit the program: ")
+        flag = int(input())
+    else:
+        print("Normal temperature monitoring: ")
+        print("Temperature value is: "+str(temperature)+"° C")
+        print("Humidity value is: "+str(humidity))
